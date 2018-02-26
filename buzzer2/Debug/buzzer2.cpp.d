c1dfe47983cb8b94a753d49aa0e7862c85f42460
Ayrton <ayrtonreis8@gmail.com>
2018-02-25 23:31:57 -0300

    Buzzer melody generator
    IDE: Visual Studio with Arduino extension.

---
+++ buzzer2/Debug/buzzer2.cpp.d
@@ -0,0 +1,14 @@
+C:\Users\Ayrton\ R\AppData\Local\Temp\VMBuilds\buzzer2\uno\Debug\buzzer2.cpp.o: \
+ C:\Users\Ayrton\ R\AppData\Local\Temp\VMBuilds\buzzer2\uno\Debug\buzzer2.cpp \
+ E:\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ E:\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
+ E:\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ E:\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
+ E:\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ E:\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ E:\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ E:\Arduino\hardware\arduino\avr\cores\arduino/Printable.h \
+ E:\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ E:\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ E:\Arduino\hardware\arduino\avr\variants\standard/pins_arduino.h \
+ C:\Users\Ayrton\ R\AppData\Local\Temp\VMBuilds\buzzer2\uno\Debug\pitches.h
---
+++ buzzer2/buzzer2.ino
@@ -0,0 +1,21 @@
+#include "pitches.h"
+
+void setup()
+{
+
+	//music_start(9);
+	//delay(2000);
+	//music_end(9);
+	//music_pirates(9);
+	music_mario(9);
+	delay(1000);
+	music_frog(9);
+
+}
+
+void loop()
+{
+
+  /* add main program code here */
+
+}
